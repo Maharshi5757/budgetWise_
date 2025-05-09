66614e5a4176e6a05569033bd11a33fef3a109d4
Maharshi5757 <maharshi5757@gmail.com>
2025-05-08 20:01:44 -0400

    progress view update

--- Budgetwise_homePage/CategoryRow.swift
+++ Budgetwise_homePage/CategoryRow.swift
@@ -2,6 +2,25 @@ import SwiftUI
 struct CategoryRow: View {
     let category: BudgetCategory
     
+    private func getCategoryColor() -> Color {
+        switch category.name.lowercased() {
+        case "food":
+            return Color(red: 33/255, green: 59/255, blue: 128/255) // #213B80
+        case "shopping":
+            return Color(red: 56/255, green: 107/255, blue: 188/255) // #386BBC
+        case "transportation":
+            return Color(red: 255/255, green: 185/255, blue: 0/255) // Yellow
+        case "education":
+            return Color(red: 70/255, green: 189/255, blue: 198/255) // #46BDC6
+        case "groceries":
+            return Color(red: 0/255, green: 188/255, blue: 56/255) // Green
+        case "housing":
+            return Color(red: 255/255, green: 55/255, blue: 60/255) // Red
+        default:
+            return Color(red: 82/255, green: 130/255, blue: 226/255) // Default Blue
+        }
+    }
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 4) {
             HStack {
@@ -37,7 +56,7 @@ struct CategoryRow: View {
             ProgressView(value: category.spent, total: category.budget)
                 .scaleEffect(x: 1, y: 2, anchor: .center)
                 .padding(.horizontal, -40)
-                .tint(.green)
+                .tint(getCategoryColor())
         }
         .padding(.vertical, 4)
     }
